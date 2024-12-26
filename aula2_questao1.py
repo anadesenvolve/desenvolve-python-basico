b741b2290800cf14fa2a65ec54c98ee7109d131c
anadesenvolve <ana.pereira@pditabira.com>
2024-12-26 15:05:48 -0300

    Add files via upload

---
+++ aula2_questao1.py
@@ -0,0 +1,14 @@
+# Armazenando os valores nas variáveis
+var1 = 5
+var2 = 2
+
+# Imprimindo o tipo de dados de cada variável
+print(type(var1))  # <class 'int'>
+print(type(var2))  # <class 'int'>
+
+# Realizando a divisão e armazenando o resultado em uma terceira variável
+resultado = var1 / var2
+
+# Imprimindo a terceira variável e seu tipo
+print(resultado)    # 2.5
+print(type(resultado))  # <class 'float'>
---
+++ aula2_questao3.py
@@ -0,0 +1,12 @@
+# Definindo as variáveis
+v1 = 10
+v2 = 20
+
+# Usando uma variável auxiliar para trocar os valores
+aux = v1
+v1 = v2
+v2 = aux
+
+# Imprimindo os novos valores das variáveis
+print(v1)  # 20
+print(v2)  # 10
