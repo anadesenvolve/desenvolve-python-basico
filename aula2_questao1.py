25b168091ba51d66abb72a59d227d4288b9402da
anadesenvolve <ana.pereira@pditabira.com>
2024-12-29 16:25:18 -0300

    Módulo 3 - Condicionais  Aula 3.2

--- aula2_questao1.py
+++ aula2_questao1.py
@@ -1,14 +1,10 @@
-# Armazenando os valores nas variáveis
-var1 = 5
-var2 = 2
 
-# Imprimindo o tipo de dados de cada variável
-print(type(var1))  # <class 'int'>
-print(type(var2))  # <class 'int'>
+idade_juliana = int(input("Digite a idade de Juliana: "))
+idade_cris = int(input("Digite a idade de Cris: "))
 
-# Realizando a divisão e armazenando o resultado em uma terceira variável
-resultado = var1 / var2
 
-# Imprimindo a terceira variável e seu tipo
-print(resultado)    # 2.5
-print(type(resultado))  # <class 'float'>
+pode_entrar = idade_juliana > 17 and idade_cris > 17
+
+
+print(pode_entrar)
+
