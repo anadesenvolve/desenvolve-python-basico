1eb2fecc3183e7a012c1d0d52105893957a99797
anadesenvolve <ana.pereira@pditabira.com>
2024-12-30 12:03:05 -0300

    Update aula4_questao2.py

---
+++ aula4_questao2.py
@@ -0,0 +1,6 @@
+
+fahrenheit = int(input("Digite a temperatura em graus Fahrenheit: "))
+
+celsius = int((fahrenheit - 32) * (5 / 9))
+
+print(f"{fahrenheit} graus Fahrenheit são {celsius} graus Celsius.")
